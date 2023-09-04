d34cda158562697b3088adc41a030100e84bd468
Timo Borer <timoborer@gmail.com>
2023-09-04 20:42:34 +0200

    fixed useCounter hook

--- interface/hooks/useCounter.tsx
+++ interface/hooks/useCounter.tsx
@@ -2,9 +2,19 @@ import React, { createContext, useContext, useState } from "react";
 
 const Context = createContext<number>(0);
 
-const useCounter = () => useContext(Context);
+const useCounter = () => {
+  const context = useContext(Context);
 
-const CounterProvider = ({ children }) => {
+  if (context === undefined) {
+    throw new Error("useCounter must be used within a CounterProvider");
+  }
+
+  const [count, setCount] = useState(context);
+
+  return [count, setCount] as const;
+}
+
+const CounterProvider = ({ children }: { children: React.ReactNode }) => {
   const [count, setCount] = useState(0);
 
   return (
