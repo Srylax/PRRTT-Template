901a7294a437acee85a2cdbef13dd9fa4153f4e7
Timo Borer <timoborer@gmail.com>
2023-09-04 20:42:55 +0200

    updated react web app

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter } from 'react-router-dom';
+import { AppInterface, routes } from "@ns/interface";
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <
+      <AppInterface router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
