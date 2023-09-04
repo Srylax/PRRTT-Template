358c3d31da86aa461dc459875ea713ba545e856f
Timo Borer <timoborer@gmail.com>
2023-09-04 20:43:02 +0200

    export app files

--- interface/index.tsx
+++ interface/index.tsx
@@ -1,5 +1,8 @@
 import { RouterProvider, RouterProviderProps } from 'react-router-dom';
 
+export * from './app';
+
+
 export const AppInterface = (props: { router: RouterProviderProps['router'] }) => {
     return (
         <RouterProvider router={props.router} />
